bad61b5ecb95560b002de86afb960a7f6b72b0a6
Marma92 <elbaz.amram@gmail.com>
2018-12-02 03:10:34 +0100

    adding a favicon

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express   = require('express');
 var app       = express();
 var port      = process.env.PORT || 8080;
 var morgan    = require('morgan');
-
-
+var favicon   = require('serve-favicon');
 
 app.use('/public/imgs', express.static('imgs'));
 app.use('/public/css', express.static('css'));
 app.use(morgan('dev'));
 app.set('view engine', 'ejs');
+app.use(favicon(__dirname + '/imgs/favicon.ico'));
 
 // routes ======================================================================
 require('./app/routes.js')(app);
